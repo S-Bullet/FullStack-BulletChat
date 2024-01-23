cfcd3fe465741581b9268d2142214c763687d16c
Tookssi <pjubullet816@gmail.com>
2024-01-23 16:05:53 +0900

    add functionality to the Join Room form

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -2,14 +2,32 @@
 
 import styles from './styles.module.css';
 
-const Home = () => {
+const Home = ({ username, setUsername, room, setRoom, socket }) => {
+    
+  // Add this
+  const joinRoom = () => {
+    if (room !== '' && username !== '') {
+      socket.emit('join_room', { username, room });
+    }
+    
+    // Redirect to /chat
+    navigate('/chat', { replace: true }); // Add this
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.formContainer}>
         <h1>{`<>DevRooms</>`}</h1>
-        <input className={styles.input} placeholder='Username...' />
+        <input 
+            className={styles.input} 
+            placeholder='Username...' 
+            onChange={(e) => setUsername(e.target.value)}
+        />
 
-        <select className={styles.input}>
+        <select 
+            className={styles.input}
+            onChange={(e) => setRoom(e.target.value)}
+        >
           <option>-- Select Room --</option>
           <option value='javascript'>JavaScript</option>
           <option value='node'>Node</option>
@@ -17,7 +35,13 @@ const Home = () => {
           <option value='react'>React</option>
         </select>
 
-        <button className='btn btn-secondary' style={{ width: '100%' }}>Join Room</button>
+        <button 
+            className='btn btn-secondary' 
+            style={{ width: '100%' }}
+            onClick={joinRoom}
+        >
+            Join Room
+        </button>
       </div>
     </div>
   );
